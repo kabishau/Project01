8f5e12358113e61df53009e75eab84787e6a184c
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2019-03-27 16:52:29 -0400

    Create DetailViewController.swift

---
+++ Project01/DetailViewController.swift
@@ -0,0 +1,15 @@
+Import UIKit
+// TODO:
+// add new view controller in Main.storyboard
+// assign the DetailViewController class
+// add UIImageView and make it fit the full screen + layout
+// connect @IBOutlet with code
+
+
+class DetailViewController {
+  @IBOutlet weak var imageView: UIImageView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+}
