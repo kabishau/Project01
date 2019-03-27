aa3fcc7dfb46af891d297cf718d094e846749724
AlekseyK <aleksey.kabishau@gmail.com>
2019-03-26 23:33:56 -0400

    initial setup

--- Project01/ViewController.swift
+++ Project01/ViewController.swift
@@ -1,20 +1,31 @@
-//
-//  ViewController.swift
-//  Project01
-//
-//  Created by Aleksey on 0326..19.
-//  Copyright © 2019 Aleksey. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let fileManager = FileManager.default
+        let path = Bundle.main.resourcePath! // path for app's bundle
+        let items = try! fileManager.contentsOfDirectory(atPath: path) // all items app's bundle
+
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+                print(pictures)
+            }
+        }
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
 }
-
